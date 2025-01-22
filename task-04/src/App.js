90192d93ababb8eb058ffb544a5226fcc2a13b2f
AlexStel <alexeistel@gmail.com>
2025-01-22 22:20:22 +0300

    feat: add solution hook useViewportSize

--- task-04/src/App.js
+++ task-04/src/App.js
@@ -1,9 +1,13 @@
 import { useViewportSize } from './useViewportSize';
 
 function Demo() {
+  const { width, height } = useViewportSize();
 
   return (
-   
+    <>
+      <p>Width: {width}</p>
+      <p>Height: {height}</p>
+    </>
   );
 }
 
